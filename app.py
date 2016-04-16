cd66e7aca827bfd7c9237fc4c0d229c758548ce6
Raghav Toshniwal <raghav.toshniwal@gmail.com>
2016-04-16 23:43:07 +0100

    Massive improvements

--- app.py
+++ app.py
@@ -8,22 +8,99 @@ This file creates your application.
 
 import os,requests
 import pickle
+import random
 from firebase import firebase
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, flash, redirect, url_for, Markup
 
 app = Flask(__name__)
 
 app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY', 'this_should_be_configured')
 
-
+answer=''
 ###
 # Routing for your application.
 ###
-
+@app.route('/<url>')
 @app.route('/')
-def home():
+def home(url='default'):
     """Render website's home page."""
-    return render_template('home.html')
+    if(url=='default'):
+        return render_template('home.html',result=url)
+    else:
+        firebase2 = firebase.FirebaseApplication('https://trimkim.firebaseio.com', None)
+        live=firebase2.get('/live', None)
+        for item in live:
+            current = live[item]['r0']+live[item]['r1']+live[item]['r2']
+            if current==url.lower:
+                if validURL(live[item]['url'])==True:
+                    return redirect(url)
+                else:
+                    url="http://"+url
+                    return redirect(url)
+        return render_template('404.html'), 404
+
+
+
+def generateThree():
+    master=pickle.load(open("mega.pickle","rb"))
+    result=[]
+    l=len(master)
+    f=random.randint(0,l)
+    mylist = list(xrange(len(master[f])))
+    random.shuffle(mylist)
+    randomlist=mylist[:3]
+    for number in randomlist:
+        result.append(master[f][number])
+    return result
+
+@app.route('/process',methods=['POST'])
+def surgeon():
+    url=request.form['link']
+    rhyme=giveValid()
+    firebase2 = firebase.FirebaseApplication('https://trimkim.firebaseio.com', None)
+    x = firebase2.post('/used', str(rhyme))
+    x = firebase2.post('/live',{'url':url,'r0':rhyme[0],'r1':rhyme[1],'r2':rhyme[2]})
+    furl=rhyme[0].title()+rhyme[1].title()+rhyme[2].title()
+    flash(Markup('Successfully shortened, please click <a href="http://trim.kim/'+furl+'" > trim.kim/'+furl+  '</a>'))
+    return redirect('/')
+
+
+
+
+@app.route('/fresh',methods=['POST'])
+def giveValid2():
+    firebase2 = firebase.FirebaseApplication('https://trimkim.firebaseio.com', None)
+    used=firebase2.get('/used', None)
+    fresh=generateThree()
+    url=str(fresh)
+    if url not in used:
+        #return str(fresh)
+        return request.form['plane']
+def validURL(url):
+    try:
+        request = requests.get(url)
+        if request.status_code == 200:
+            return True
+        else:
+            return False
+    except:
+        return False
+def giveValid():
+    firebase2 = firebase.FirebaseApplication('https://trimkim.firebaseio.com', None)
+    used=['null']
+    used=firebase2.get('/used', None)
+    fresh=generateThree()
+    url=str(fresh)
+    if url not in used:
+        return fresh
+    else:
+        return getValid() #such a horrible decision :(   
+    
+@app.route('/wt')
+def wt():
+    firebase2 = firebase.FirebaseApplication('https://trimkim.firebaseio.com', None)
+    result = firebase2.post('/test', 'new_user')
+    return str(result)
 
 @app.route('/whoops')
 def whoops():
@@ -31,7 +108,7 @@ def whoops():
     total=0
     for wordlen in master:
         total=total+len(wordlen)
-        print wordlen
+        #print wordlen
     print total/2544
     return str(master)
 
